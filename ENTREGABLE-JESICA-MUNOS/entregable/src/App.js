8887636c5de9918b002bfd6927f1dfd672749c1e
jesyn <jesydynathos@gmail.com>
2022-09-01 14:05:39 -0300

    tp finalizado

--- ENTREGABLE-JESICA-MUNOS/entregable/src/App.js
+++ ENTREGABLE-JESICA-MUNOS/entregable/src/App.js
@@ -2,12 +2,6 @@ import Cabecera from "./components/Cabecera";
 import Listado from "./components/Listado";
 import { useState } from 'react';
 
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
 function App() {
   const [itemComprado, setItemComprado] = useState(0)
--- ENTREGABLE-JESICA-MUNOS/entregable/src/components/Cabecera.js
+++ ENTREGABLE-JESICA-MUNOS/entregable/src/components/Cabecera.js
@@ -1,12 +1,5 @@
 import React from 'react'
 
-// El componente Cabera no tiene componentes hijos.
-// ESTADO: Cabecera no tiene estado.
-// MÉTODOS: Cabecera no tiene métodos.
-// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
-// Maqueta de Cabecera:
-//    h1
-//    p > span     (el span mostrará la cantidad recibida por props)
 
 export default function Cabecera({itemComprado}) {
   
