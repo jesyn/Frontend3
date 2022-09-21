59eebe74df24843a164c8bbf570665e18db51ee4
Jesica Muñoz <jesydynathos@gmail.com>
2022-09-20 21:42:06 -0300

    cambio de titulo

--- CLASE22-hookPersonalizado/hooks/src/App.js
+++ CLASE22-hookPersonalizado/hooks/src/App.js
@@ -7,7 +7,7 @@ function App() {
     
   return (
     <div className='container'>
-      <h1>usando toggle</h1>
+      <h1>using toggle</h1>
       <h2>{isTextChanged ? 'one text' : 'two text'}</h2>
       <button onClick={setIsTextChanged}>change text</button>
     </div>
